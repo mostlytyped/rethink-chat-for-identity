5104488ffbb613bd9eea49b9003ecd233dcf95a4
Paddy <ohanlon.paddy@gmail.com>
2022-04-28 12:56:37 +0200

    Compile sass with Webpack

--- README.md
+++ README.md
@@ -12,7 +12,4 @@ npm install
 npm run serve # start the Express server static files
 npm run build-watch # use Webpack to build app.js, the Vue app
 npm run build # use Webpack to build for prod
-
-# Optional
-sass --watch public/css:public/css
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require("path");
 const webpack = require("webpack");
 const dotenv = require("dotenv");
 const CopyPlugin = require("copy-webpack-plugin");
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
 
 module.exports = {
-    entry: "./src/app.js",
+    entry: ["./src/app.js", "./src/scss/main.scss"],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
@@ -14,6 +15,21 @@ module.exports = {
             querystring: require.resolve("querystring-es3"),
         },
     },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+
+                type: "asset/resource",
+                generator: {
+                    filename: "css/[name].css",
+                },
+
+                use: ["sass-loader"],
+            },
+        ],
+    },
     plugins: [
         new webpack.DefinePlugin({
             "process.env": JSON.stringify(dotenv.config().parsed),
@@ -21,5 +37,6 @@ module.exports = {
         new CopyPlugin({
             patterns: [{ from: "public", to: "." }],
         }),
+        new RemoveEmptyScriptsPlugin(),
     ],
 };
