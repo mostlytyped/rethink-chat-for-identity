a25caa7601412b8850aa024b6ad217e2edb9c8e0
Paddy <ohanlon.paddy@gmail.com>
2022-05-04 18:32:25 +0200

    Make this app at least somewhat useable

--- src/rethinkid/index.js
+++ src/rethinkid/index.js
@@ -1,11 +1,12 @@
-import router from "../router";
-
 const baseURL = window.location.origin;
 
 const config = {
     appId: "6d85d59f-d167-4724-acbb-39090821593e",
     signUpRedirectUri: baseURL,
     logInRedirectUri: `${baseURL}/callback`,
+    dataAPIConnectErrorCallback: function (errorMessage) {
+        this.logOut();
+    },
 };
 
 // `RethinkID` is imported via a script tag in index.html
